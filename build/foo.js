43e51e16ef67211d3be77ca02cea7b76485d0626
Imaru <irene_marrero_ruiz@hotmail.com>
2021-02-24 13:40:52 +0000

    First session

---
+++ build/foo.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log('Hola, mundo');
+var userName;
+var email;
+var age;
+var toggle = function (propertie) { return propertie = !propertie; };
---
+++ src/app.ts
@@ -0,0 +1,7 @@
+console.log('Hola, mundo');
+
+let userName:string
+let email:string 
+let age:number
+
+const toggle = (propertie:boolean) =>  propertie = !propertie
